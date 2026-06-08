4ade0f8f6e606f979ab8bfb93681c90e038a0bb7
Ahihidongok1012 <vymoon1012@gmail.com>
2026-06-08 21:43:28 +0700

    Implement genre extraction in genre.js
    Added genre extraction functionality from the website.

---
+++ dualeotruyen/src/genre.js
@@ -0,0 +1,22 @@
+load('config.js');
+
+function execute() {
+    let doc = getDoc('https://dualeotruyenbs.com/');
+    let data = [];
+    let seen = {};
+
+    if (doc) {
+        doc.select('nav a[href*="/the-loai/"], .menu a[href*="/the-loai/"]').forEach(function(e) {
+            let link = normalizeUrl(e.attr('href') || '');
+            if (!link || seen[link]) return;
+            seen[link] = true;
+            let name = cleanText(e.text());
+            if (name) data.push({ title: name, input: link, script: 'gen.js' });
+        });
+    }
+
+    if (data.length === 0) {
+        data = [{ title: 'Tất cả', input: 'https://dualeotruyenbs.com/', script: 'gen.js' }];
+    }
+    return Response.success(data);
+}
